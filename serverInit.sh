a7f05ed75e95fcb2a5f5fe131307eb736e94fa6a
globedasher <globedasher@loadbearer.homebase>
2017-02-03 14:14:06 -0800

    further updates to the serverInit script

--- serverInit.sh
+++ serverInit.sh
@@ -1,42 +1,59 @@
 #!/bin/bash
 
-sudo mv nginx.conf /etc/nginx/sites-available/fullstack
-
+# Add the key and move a sources list into the apt directory to add access to
+# the MongoDB repositories for Ubuntu.
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 sudo mv mongodb-org-3.4.list /etc/apt/sources.list.d/
 
+# Update and install all needed applications.
 sudo apt-get update
-sudo apt-get install -y build-essential openssl libssl-dev pkg-config
+sudo apt-get install -y build-essential \
+			openssl 	\
+			libssl-dev 	\
+			pkg-config 	\
+			tmux 		\
+			htop 		\
+			tree 		\
+			nginx 		\
+			git 		\
+			npm		\
+			mongodb-org	\
 
-
-sudo apt-get install -y npm
+# Clean npm cache
 sudo npm cache clean -f
 
-sudo npm install -g -y n pm2
+# Install n, project manager and bower globally
+sudo npm install -g -y n pm2 bower
+# And latest version of node
 sudo n stable
 
+echo"Installs complete. Moving on to configuration..."
 
-sudo apt-get install -y nginx
-sudo apt-get install -y git
+# Move the nginx configuration file to the sites available/{{ PROJECT_NAME}}
+# location
+sudo mv nginx.conf /etc/nginx/sites-available/fullstack
 
+# Clone MEAN project from github location to /var/www/{{ PROJECT_NAME }}
 sudo git clone https://github.com/globedasher/fullstack /var/www/fullstack
-# I need to install node_modules and bower_components after this step
 
+# Move to the project file and install project dependencies
 cd /var/www/fullstack
 sudo npm install -y
-sudo npm install -g bower
 sudo bower install --allow-root
 
+# Remove nginx default site files.
 sudo rm /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/default
 
+# Create symlink to newly avaible project.
 sudo ln -s /etc/nginx/sites-available/fullstack /etc/nginx/sites-enabled/fullstack
 
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+# Ensure mongod is started.
 sudo service mongod start
 
+# Move to the project directory and start the project.
 cd /var/www/fullstack
 pm2 start server.js
+# Reload and restart nginx.
 sudo service nginx reload && sudo service nginx restart
 echo "Complete. Please check all components installed properly."
